2384d4ed4bba2a192345cb0f61ebbde62caf5e0c
cNoNim <my@cnonim.name>
2024-01-31 02:58:29 +0300

    Move files to build prefix

--- recipe/install_cmake.bat
+++
@@ -1,4 +0,0 @@
-@echo ON
-setlocal enabledelayedexpansion
-
-mkdir "%LIBRARY_PREFIX%\lib\cmake\Glad\" && copy /Y "%SRC_DIR%\cmake\CMakeLists.txt" "%LIBRARY_PREFIX%\lib\cmake\Glad\GladConfig.cmake"
--- recipe/install_cmake.sh
+++ recipe/install_cmake.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ex
 
-mkdir -p $PREFIX/lib/cmake/Glad && cp $SRC_DIR/cmake/CMakeLists.txt $PREFIX/lib/cmake/Glad/GladConfig.cmake
+mkdir -p $BUILD_PREFIX/lib/cmake/Glad && cp $SRC_DIR/cmake/CMakeLists.txt $BUILD_PREFIX/lib/cmake/Glad/GladConfig.cmake
+mkdir -p $BUILD_PREFIX/Library/lib/cmake/Glad && cp $SRC_DIR/cmake/CMakeLists.txt $BUILD_PREFIX/Library/lib/cmake/Glad/GladConfig.cmake
