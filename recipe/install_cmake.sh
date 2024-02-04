cb48dc5e0a56e397cea43bef2f73c4fed95560ed
cNoNim <my@cnonim.name>
2024-02-04 20:56:44 +0300

    Remove old patches and add tests by cmake

--- recipe/install_cmake.sh
+++ recipe/install_cmake.sh
@@ -2,4 +2,4 @@
 set -ex
 
 mkdir -p $PREFIX/lib/cmake/Glad
-cp $SRC_DIR/cmake/CMakeLists.txt $PREFIX/lib/cmake/Glad/GladConfig.cmake
+cp $SRC_DIR/cmake/GladConfig.cmake $PREFIX/lib/cmake/Glad/GladConfig.cmake
